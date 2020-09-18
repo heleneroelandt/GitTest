d7fabbf65fa24e353abce8625eb43bb23f08bf97
Hélène Roelandt <heleneroelandt@MacBook-Pro-van-Helene.local>
2020-09-18 10:59:14 +0200

    added file

---
+++ TestGithub.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 10:52:08 2020
+
+@author: heleneroelandt
+"""
+
+a = 1
+b = 2
+c =  a+b
+print(c)
+print("GitHub getest")
